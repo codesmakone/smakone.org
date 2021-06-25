a90a9c877d71150b89f8c8327183fb62b64f2e99
Lefrozte <69727489+Lefrozte@users.noreply.github.com>
2021-06-25 21:22:32 +0700

    Navbar

---
+++ src/components/header-mobile/HeaderMobile.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import NavbarMobileContainer from "../navbarmobile/NavbarMobileContainer";
+
+import SMAK1 from '../../images/SMAK1PENABURJakarta.png';
+import './Checked.css';
+
+function Header() {
+  return (
+    <header className="bg-white lg:px-16 px-6 bg-white flex flex-wrap items-center lg:py-0 py-2 fixed z-50 w-full shadow-lg top-0">
+        <div className="flex-1 flex justify-between items-center">
+          <Link to="/">
+            <img className="object-scale-down h-5" src={SMAK1} alt="OSIS SMAK 1 Logo" />
+          </Link>
+        </div>
+        <label htmlFor="menu-toggle" className="pointer-cursor lg:hidden block">
+          <svg className="fill-current text-gray-900" xmlns="http://www.w3.org/2000/svg" width={20} height={20} viewBox="0 0 20 20">
+            <title>menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg>
+        </label>
+        <input className="hidden" type="checkbox" id="menu-toggle" />
+        <NavbarMobileContainer></NavbarMobileContainer>
+
+        
+      </header>
+  );
+}
+
+export default Header
--- src/components/navbar/NavbarContainer.js
+++ src/components/navbar/NavbarContainer.js
@@ -1,26 +1,51 @@
 import React from 'react'
 import { Link } from 'gatsby';
-
+import SMAK1 from '../../images/SMAK1PENABURJakarta.png'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 function NavbarContainter() {
+  const Navbar = ({ toggle }) => {
     return (
-        <div className="hidden lg:flex lg:items-center lg:w-auto w-full py-7" id="menu">
-          <nav>
-            <ul className="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0">
-              <li><Link className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-gray-400" to="/about">Tentang Kami</Link></li>
-              <li><Link className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-gray-400" to="#!">Kehidupan Smukierz</Link></li>
-              <li><Link className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-gray-400" to="/kegiatan">Kegiatan</Link></li>
-              <li><Link className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-gray-400" to="#!">Berita</Link></li>
-              <li><Link className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-gray-400 lg:mb-0 mb-2" to="/alumni">Alumni</Link></li>
-            </ul>
-          </nav>
-          <Link href="#!" className="lg:ml-4 flex items-center justify-start lg:mb-0 mb-4 pointer-cursor">
-            <FontAwesomeIcon icon={faSearch} size="lg" className="rounded-full border-2 border-transparent hover:bor-gray-400" alt="Search Logo" />
-          </Link>
+      <nav
+        className='flex justify-between items-center h-16 bg-white text-black relative shadow-sm font-mono'
+        role='navigation'
+      >
+        <Link to='/' className='pl-8'>
+          <img className="object-scale-down h-5" src={SMAK1} alt="OSIS SMAK 1 Logo" />
+        </Link>
+        <div className='px-4 cursor-pointer md:hidden' onClick={toggle}>
+          <svg
+            className='w-8 h-8'
+            fill='none'
+            stroke='currentColor'
+            viewBox='0 0 24 24'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <path
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              strokeWidth='2'
+              d='M4 6h16M4 12h16M4 18h16'
+            />
+          </svg>
         </div>
+        <div className='pr-8 md:block  hidden'>
+          <ul className="lg:flex items-center justify-between text-base text-gray-700 pt-4 lg:pt-0">
+            <li><Link className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-gray-400" to="/about">Tentang Kami</Link></li>
+            <li><Link className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-gray-400" to="#!">Kehidupan Smukierz</Link></li>
+            <li><Link className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-gray-400" to="/kegiatan">Kegiatan</Link></li>
+            <li><Link className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-gray-400" to="#!">Berita</Link></li>
+            <li><Link className="lg:p-4 py-3 px-0 block border-b-2 border-transparent hover:border-gray-400 lg:mb-0 mb-2" to="/alumni">Alumni</Link></li>
+          </ul>
+        </div>
+        <Link href="#!" className="lg:ml-4 flex items-center justify-start lg:mb-0 mb-4 pointer-cursor">
+            <FontAwesomeIcon icon={faSearch} size="lg" className="rounded-full border-2 border-transparent hover:bor-gray-400" alt="Search Logo" />
+        </Link>
+      </nav>
+      
     );
+  };
 }
 
-export default NavbarContainter
+export default NavbarContainter;
