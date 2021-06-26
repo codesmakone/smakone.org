716639c6d5aa7842ab66fd9bf5c270056e1abd81
Sean Michael Suntoso <ssuntoso@gmail.com>
2021-06-26 22:58:59 +0700

    Update

--- src/components/home/homeCard.js
+++ src/components/home/homeCard.js
@@ -3,8 +3,8 @@ import { Link } from 'gatsby';
 
 function HomeCard(props) {
     return(
-        <div className="m-3 p-3 shadow-2xl rounded-xl bg-white">
-            <img src={ props.img } className="object-contain rounded-lg" alt="Billy" />
+        <div className="m-3 shadow-2xl rounded-xl bg-white">
+            <img src={ props.img } className="object-contain rounded-t-xl" alt="Billy" />
             <div className="p-2">
                 <p className="text-center">{ props.desc }</p>
                 <h2 className="font-medium text-center text-lg mb-2">{ props.title }</h2>
