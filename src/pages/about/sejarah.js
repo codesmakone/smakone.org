6180e948f90e8c63dd7fd707cfc18fcb2a36dede
Ferdi123YT <63623479+Ferdi123YT@users.noreply.github.com>
2021-06-28 00:32:10 +0700

    sejarah

--- src/pages/about/sejarah.js
+++ src/pages/about/sejarah.js
@@ -7,14 +7,48 @@ export default function HistoryPage() {
     const title = "Sejarah, Visi, dan Misi SMAKONE"
     const content = (
         <Content>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In porttitor nulla vel accumsan blandit. Mauris luctus sem non quam vestibulum, non placerat ipsum venenatis. Nam vitae felis et lectus porttitor sagittis. Nunc sit amet metus ornare, auctor lorem at, congue orci. Aliquam tempus tellus diam, quis viverra felis laoreet ac. Vestibulum at dolor ut arcu dictum tincidunt. Pellentesque et justo nec nibh tincidunt bibendum nec quis elit. Nullam porttitor magna semper, cursus odio quis, tincidunt purus. Aenean mattis magna in sapien rhoncus finibus. Quisque eleifend lacus sit amet mollis posuere. Maecenas efficitur eros viverra dui lacinia, viverra tristique eros semper.
-
-Aliquam sollicitudin elementum dui, vitae dapibus neque efficitur ut. Fusce mattis libero dolor, sit amet mollis purus auctor eget. Cras justo purus, venenatis nec eros semper, pulvinar consectetur velit. Vivamus dignissim sodales lorem a eleifend. Nullam aliquet nulla vel neque auctor rutrum. Mauris ut quam condimentum, efficitur mauris sed, porta magna. Proin vitae ante quis justo porta volutpat id vitae tortor. Curabitur mattis mi in est blandit, ac ultrices turpis dictum. Suspendisse vel felis nisi. Vivamus id turpis mollis, consequat ipsum in, consequat turpis. Nullam tempor elementum nunc non facilisis. Praesent sed massa ac libero rhoncus elementum et et purus. Donec molestie, ex eu pellentesque rhoncus, erat lectus finibus risus, ut gravida odio velit blandit nunc.
-
-Sed molestie tortor in eros pulvinar laoreet. Nulla porttitor ligula sed mollis lacinia. Nam lorem mi, pellentesque eu placerat nec, ultricies vel velit. Suspendisse pretium magna viverra velit sodales blandit. Vivamus eu justo diam. Donec eleifend efficitur diam. Maecenas id ipsum urna. Praesent at lorem rhoncus mauris faucibus iaculis nec a nulla. Pellentesque eu orci id neque efficitur sagittis. Duis ut malesuada tortor, ut tempus libero. Mauris iaculis ut ante sed volutpat. Nulla posuere pharetra lorem, id dignissim ante laoreet eu. In hac habitasse platea dictumst. Aenean non metus mollis, consectetur lorem eget, pharetra felis. Suspendisse in ullamcorper elit.
-
-In hac habitasse platea dictumst. Nullam ut elit tincidunt, pretium lacus et, fringilla odio. Sed commodo purus sed dolor hendrerit, ac dignissim ipsum accumsan. In in posuere metus. Maecenas quis turpis velit. Duis bibendum sem sed orci porttitor, eu sodales ipsum consequat. Vestibulum aliquet semper mattis. Donec metus neque, tristique sit amet lacus non, ornare laoreet orci. Morbi molestie euismod est, ut lacinia lacus varius sed. Vestibulum dolor sapien, vestibulum sed elementum in, pulvinar eget lacus. Aliquam nec ipsum ut tellus varius pharetra vel in. (400 Word)
-            </Content>
+            <h2 className="justify-left text-left font-bold text-lg pt-4 pb-2">
+                Visi SMAK 1:
+            </h2>
+            <p className="justify-center text-justify p-2">
+            Mewujudkan sumber daya berkualitas unggul dalam ilmu pengetahuan dan teknologi 
+            serta peduli terhadap sesama berdasarkan nilai-nilai hidup Kristiani.
+            </p>
+            <h2 className="justify-left text-left font-bold text-lg pt-4 pb-2">
+                Misi SMAK 1:
+            </h2>
+            <p className="justify-center text-justify p-2">
+            - Menyiapkan calon inventor / ilmuwan masa depan yang menguasai ilmu pengetahuan dan teknologi, kreatif, inovatif, mandiri, dan proaktif serta mempunyai landasan iman yang teguh <br />
+            - Meningkatkan profesionalisme tenaga kependidikan sesuai perkembangan dunia pendidikan
+            </p>
+            <h2 className="justify-left text-left font-bold text-lg pt-4 pb-2">
+                History:
+            </h2>
+            <p className="justify-center text-justify p-2">
+            SMAK 1 PENABUR Jakarta didirikan pada tahun 1947 sebagai MULO (Meer Uitgebreid Lager Onderwijs, 
+            sekolah menengah Kolonial Belanda) di lokasi lamanya di Jalan Pintu Air 11 (kemudian dikenal sebagai Sluis Brugstraat 11). 
+            Dipimpin oleh J.C. Boss (1947–1950), seorang ahli matematika. Pada tahun berikutnya, sekolah tersebut berganti nama menjadi 
+            CVS (Christelijke Vereniging Scholen, Christian Association School), dan kepemimpinannya diambil alih oleh Dr. Van Der Meer (1950–1951).
+            </p>
+            <p className="justify-center text-justify p-2">
+            Menyusul berakhirnya penjajahan Belanda di Indonesia pada tahun 1945, CVS diserahkan kepada THKTKH (Tiong Hoa Kie Tok Kauw Hwee, 
+            Persatuan Kristen Tionghoa) dan berganti nama menjadi SMA Istimewa (Sekolah Menengah Luar Biasa). Saat itu, pembelajaran dilakukan dalam bahasa Mandarin dan bahasa Indonesia.
+            </p>
+            <p className="justify-center text-justify p-2">
+            Pada tanggal 1 Agustus 1950, SMA Istimewa diambil alih oleh Yayasan BPK Jabar, dan sekolah tersebut mengalami pembenahan besar-besaran. 
+            Salah satu perubahan tersebut adalah penyesuaian namanya menjadi SMAK 1 (Sekolah Menengah Atas Kristen 1) dan penggunaan bahasa Indonesia sebagai satu-satunya bahasa pengantar. 
+            Penambahan kata "Kristen" (Kristen) dilakukan untuk mempertegas akar dan nilai-nilai Kristiani.
+            </p>
+            <p className="justify-center text-justify p-2">
+            Pada tahun 1956, sekolah tersebut terpilih <b>sebagai sekolah percontohan</b> untuk pengembangan sekolah-sekolah lain di Jakarta. 
+            Pada tahun yang sama SMAK 1 juga mencatatkan rekor tertinggi Ujian Nasional Fisika dan Kimia Indonesia, <b>prestasi yang diraih hingga saat ini</b>.
+            </p>
+            <p className="justify-center text-justify p-2">
+            Pada bulan Maret 1989, BPK Jabar KPS Jakarta berganti nama menjadi BPK PENABUR KPS Jakarta. Pada tahun yang sama SMAK 1 dipindahkan ke lokasi saat ini di Jalan Tanjung Duren Raya. 
+            Sekolah ini memiliki kantin, lapangan olahraga, laboratorium sains, perpustakaan, auditorium, laboratorium bahasa, ruang musik dan dansa, rumah kaca, pusat konseling siswa, ruang OSIS, 
+            aula pertemuan besar (untuk 1000 orang), dua puluh tujuh ruang kelas, dan berbagai fasilitas lainnya.
+            </p>
+        </Content>
     )
     return (
         <AboutLayout 
