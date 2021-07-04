aa374c4216343c0ba836b9e4b3c4c6ce6bd64af7
Ferdi123YT <63623479+Ferdi123YT@users.noreply.github.com>
2021-07-04 21:32:51 +0700

    ttg osis_2

--- src/pages/osis/index.js
+++ src/pages/osis/index.js
@@ -22,42 +22,32 @@ export default function OsisPage() {
                 Jalan yang akan dilewati bebas dari hambatan untuk OSIS SMAK 1 terus melebarkan sayapnya. 
             </p>
             <br />
-            <div class="grid grid-cols-2 gap-4">
-                <div>
-                    <h2 className="font-medium text-lg lg:text-5xl mt-12 justify-center text-center">
-                        Visi Kami
-                    </h2>
-                </div>
-                <div>
-                    <h2 className="font-medium text-lg lg:text-5xl mt-12 justify-center text-center">
-                        Misi Kami
-                    </h2>
-                </div>
-                <div>
-                    <p className="justify-center text-center p-2">
-                        Meningkatkan eksistensi SMAK 1 PENABUR melalui pengembangan prestasi, potensi, dan bakat peserta didik SMAK 1 PENABUR, baik di bidang akademik maupun non-akademik, 
-                        sekaligus membangun karakter yang berlandaskan iman, ilmu, dan pelayanan, serta mengedepankan pemanfaatan teknologi modern dan terwujudnya cita-cita sekaligus aspirasi warga SMAK 1 PENABUR
-                    </p>
-                </div>
-                <div>
-                    <p className="justify-center text-center p-2">
-                        Memperkenalkan serta memberikan kesempatan bagi seluruh peserta didik untuk menunjukkan kemampuan, talenta, dan hasil karya dalam program yang diadakan OSIS
-                    </p>
-                    <p className="justify-center text-center p-2">
-                        Membantu menjaga kesehatan mental dan kesehatan fisik, serta membangun karakter positif bagi warga SMAK 1 PENABUR
-                    </p>
-                    <p className="justify-center text-center p-2">
-                        Meningkatkan keterampilan dan kesiapan dalam era modern, serta menginspirasi seluruh warga SMAK 1 PENABUR untuk selalu melangkah ke depan
-                    </p>
-                    <p className="justify-center text-center p-2">
-                        Menjawab setiap aspirasi yang telah disampaikan serta merealisasikan dengan baik seluruh program OSIS sebagai bentuk nyata berjalannya OSIS yang penuh tanggung jawab
-                    </p>
-                    <p className="justify-center text-center px-2 pt-2 pb-12">
-                        Menciptakan relasi yang baik dan harmonis antar seluruh warga SMAK 1 PENABUR disertai peran OSIS sebagai penghubungnya
-                    </p>
-                </div>
+            <h2 className="font-medium text-lg lg:text-5xl mt-12 justify-center text-center">
+                Visi Kami
+            </h2>
+            <p className="justify-center text-center p-2">
+                Meningkatkan eksistensi SMAK 1 PENABUR melalui pengembangan prestasi, potensi, dan bakat peserta didik SMAK 1 PENABUR, baik di bidang akademik maupun non-akademik, 
+                sekaligus membangun karakter yang berlandaskan iman, ilmu, dan pelayanan, serta mengedepankan pemanfaatan teknologi modern dan terwujudnya cita-cita sekaligus aspirasi warga SMAK 1 PENABUR
+            </p>
+            <h2 className="font-medium text-lg lg:text-5xl mt-12 justify-center text-center">
+                Misi Kami
+            </h2>
+            <p className="justify-center text-center p-2">
+                Memperkenalkan serta memberikan kesempatan bagi seluruh peserta didik untuk menunjukkan kemampuan, talenta, dan hasil karya dalam program yang diadakan OSIS
+            </p>
+            <p className="justify-center text-center p-2">
+                Membantu menjaga kesehatan mental dan kesehatan fisik, serta membangun karakter positif bagi warga SMAK 1 PENABUR
+            </p>
+            <p className="justify-center text-center p-2">
+                Meningkatkan keterampilan dan kesiapan dalam era modern, serta menginspirasi seluruh warga SMAK 1 PENABUR untuk selalu melangkah ke depan
+            </p>
+            <p className="justify-center text-center p-2">
+                Menjawab setiap aspirasi yang telah disampaikan serta merealisasikan dengan baik seluruh program OSIS sebagai bentuk nyata berjalannya OSIS yang penuh tanggung jawab
+            </p>
+            <p className="justify-center text-center px-2 pt-2 pb-12">
+                Menciptakan relasi yang baik dan harmonis antar seluruh warga SMAK 1 PENABUR disertai peran OSIS sebagai penghubungnya
+            </p>
             </div>
-            </div>    
         </Content>
     )
     return (
