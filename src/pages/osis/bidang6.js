485b4952060d9aac4152750d55d3610487b730c8
Lefrozte <69727489+Lefrozte@users.noreply.github.com>
2021-07-04 21:26:53 +0700

    adad

---
+++ src/pages/osis/bidang6.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import OsisLayout from '../../components/page_template/OsisLayout'
+import Content from '../../components/text_helper/Content'
+import Banner from "../../images/banner2.jpg"
+
+
+export default function bidang6() {
+    const title = "Deskripsi Bidang-bidang OSIS SMAK 1 PENABUR Jakarta"
+    const content = (
+        <Content>
+            BPH: lorem ipsum <br></br>
+            Bidang 1: lorem ipsum <br></br>
+            Bidang 2: lorem ipsum <br></br>
+            Bidang 3: lorem ipsum <br></br>
+            Bidang 4: lorem ipsum <br></br>
+            Bidang 5: lorem ipsum <br></br>
+            Bidang 6: lorem ipsum <br></br>
+            Bidang 7: lorem ipsum <br></br>
+            Bidang 8: lorem ipsum <br></br>
+            Bidang 9: lorem ipsum <br></br>
+            Bidang 10: lorem ipsum <br></br>
+            </Content>
+    )
+    return (
+        <OsisLayout 
+            hero = {Banner}
+            title= {title}
+            content={content}/>
+    )
+}
