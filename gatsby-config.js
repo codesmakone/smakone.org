45ec5de9449506ecbfbe708b11d59cb03e30d135
Ferdi123YT <63623479+Ferdi123YT@users.noreply.github.com>
2021-12-25 12:34:21 +0700

    askajsk

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,7 +30,7 @@ module.exports = {
     },
   'gatsby-plugin-postcss',
   `gatsby-plugin-image`,
-  `gatsby-plugin-sharp`,
-  `gatsby-transformer-sharp`
+  // `gatsby-plugin-sharp`,
+  // `gatsby-transformer-sharp`
 ]
 };
