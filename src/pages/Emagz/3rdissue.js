358e5c49ab06a6dacd5bcd8904925a0589c629e2
Ferdi123YT <63623479+Ferdi123YT@users.noreply.github.com>
2022-01-26 12:02:54 +0700

    Create 3rdissue.js

---
+++ src/pages/Emagz/3rdissue.js
@@ -0,0 +1,29 @@
+import React from "react";
+import EmagzLayout from "../../components/page_template/EmgazLayout";
+
+import Banner from "../../images/banner2.jpg";
+
+export default function NovemberPage() {
+  const title = "E-Magazine";
+  const content = (
+    <div className="container mx-auto">
+      <h1 className="justify-center text-center p-2 font-medium text-5xl mb-5">
+        Square One
+      </h1>
+      <div className="mb-8 iframe-container">
+        <iframe
+          src="https://heyzine.com/flip-book/6b4815b101.html"
+          title="Square One"
+          width="1000"
+          height="700"
+          frameborder="0"
+          webkitAllowFullScreen
+          mozallowfullscreen
+          allowFullScreen
+          sandbox="allow-forms allow-scripts allow-same-origin allow-top-navigation"
+        ></iframe>
+      </div>
+    </div>
+  );
+  return <EmagzLayout hero={Banner} title={title} content={content} />;
+}
